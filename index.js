ecdcef0c3324de9e0b466289122bd2f7be1d4daa
th0mas2118 <thomaspierson32@gmail.com>
2023-02-19 18:19:35 +0100

    add covid

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
+function csvJSON(csv) {
+  var lines = csv.split('\n')
+  var result = []
+  var headers = lines[0].split(',')
+  for (var i = 1; i < lines.length; i++) {
+    var obj = {}
+    var currentline = lines[i].split(',')
+    for (var j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j]
+    }
+    result.push(obj)
+  }
+  return JSON.stringify(result) //JSON
+}
+
+//GESTION IP ET DEPARTEMENT
+let departement
 const location = await fetch('http://ip-api.com/json/')
   .then((response) => response.json())
   .then((data) => {
+    departement = data.zip.substring(0, 2)
     if (data.city !== 'Nancy') {
       return { lat: 48.683187984248235, lon: 6.161876994260524 }
     }
     return { lat: data.lon, lon: data.lat }
+  })
+
+//récupération donnée covid
+let hosp = []
+let dchosp = []
+let rea = []
+let dateCovid = []
+let date = `${new Date().getFullYear()}-01-01`
+await fetch('https://www.data.gouv.fr/fr/datasets/r/5c4e1452-3850-4b59-b11c-3dd51d7fb8b5')
+  .then((response) => response.text())
+  .then((response) => {
+    let data = JSON.parse(csvJSON(response))
+    data.forEach((element) => {
+      if (element.dep == departement && element.date >= date) {
+        // console.log(element)
+        rea.push(element.rea)
+        hosp.push(element.hosp)
+        dchosp.push(element.dchosp)
+        dateCovid.push(element.date)
+      }
+    })
   })
 
 //MAP CIRCULATION
@@ -39,3 +78,71 @@ fetch('https://carto.g-ny.org/data/cifs/cifs_waze_v2.json')
         .bindPopup(`${element.description}<br>Début: ${new Date(element.starttime).toLocaleDateString()}<br>Fin: ${new Date(element.endtime).toLocaleDateString()}`)
     })
   })
+
+const hospitalisation = document.getElementById('myChart1')
+const reanimation = document.getElementById('myChart2')
+const dc_hospitalisation = document.getElementById('myChart3')
+
+new Chart(hospitalisation, {
+  type: 'line',
+  data: {
+    labels: dateCovid,
+    datasets: [
+      {
+        label: 'Hospitalisation',
+        data: hosp,
+        borderWidth: 1,
+        borderColor: 'rgb(255, 0, 0)',
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+})
+new Chart(reanimation, {
+  type: 'line',
+  data: {
+    labels: dateCovid,
+    datasets: [
+      {
+        label: 'Réanimation',
+        data: rea,
+        borderWidth: 1,
+        borderColor: 'rgb(0 ,0, 255)',
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+})
+new Chart(dc_hospitalisation, {
+  type: 'line',
+  data: {
+    labels: dateCovid,
+    datasets: [
+      {
+        label: "Décès à l'hopital",
+        data: dchosp,
+        borderWidth: 1,
+        borderColor: 'rgb(0, 0, 0)',
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+})
